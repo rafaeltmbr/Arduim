fd278c5240a116a21d7f6a38c8a71376651209b0
Rafhael Payão <rafaeltmbr@gmail.com>
2019-06-10 19:24:05 -0300

    change print strings

--- test/test_lcd.c
+++ test/test_lcd.c
@@ -8,8 +8,8 @@ void setup()
     lcd = lcd_get(C6, C7, LCD_PORTD);
     lcd_select(&lcd);
 
-    lcd.setCursor(0, 0);
-    lcd.prints("  Debugging LCD ");
+    lcd.setCursor(2, 0);
+    lcd.prints("Debugging LCD");
     lcd.setCursor(0, 1);
     lcd.prints("file: test_lcd.c");
     delay(1000);
@@ -19,9 +19,9 @@ void loop()
 {
     static uint16_t count = 0;
     lcd.clear();
-    lcd.setCursor(0, 5);
+    lcd.setCursor(5, 0);
     lcd.prints("lcd.h");
-    lcd.setCursor(0, 3);
+    lcd.setCursor(3, 1);
     lcd.prints("count: ");
     lcd.printi(count++);
     delay(500);
